5cb749f6d709307b5546a31e2f2fca2794fa4f90
Tomicarrie <tcarrie@udesa.esc.edu.ar>
2026-04-24 11:54:12 -0300

    agrego cambios en database.js para tener la bbdd en supabase

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 const { env } = require('./env');
 
+const SSL_CONFIG = { rejectUnauthorized: false };
+
 const pool = new Pool({
   host: env.DB_HOST,
   port: parseInt(env.DB_PORT, 10),
   database: env.DB_NAME,
   user: env.DB_USER,
   password: env.DB_PASSWORD,
+  ssl: SSL_CONFIG,
 });
 
 async function query(text, params) {
